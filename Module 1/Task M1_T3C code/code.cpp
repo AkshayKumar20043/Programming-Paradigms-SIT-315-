bbde34dc97396d6cb504fbbdc5d0e7771e355bd0
AkshayKumar20043 <141328267+AkshayKumar20043@users.noreply.github.com>
2024-02-10 15:35:45 +0530

    Update code.cpp

--- Module 1/Task M1_T3C code/code.cpp
+++ Module 1/Task M1_T3C code/code.cpp
@@ -1,54 +1,36 @@
-int ledPin1 = 6; // LED connected to digital pin 6
-int pirPin1 = 3; // PIR sensor 1 connected to digital pin 3
-int ledPin2 = 7; // LED connected to digital pin 7
-int pirPin2 = 2; // PIR sensor 2 connected to digital pin 2
+const int motionSensorPin = 3;
+const int temperatureSensorPin = A1;
+const int ledPin = 6;
 
-void setup()
-{
-  pinMode(ledPin1, OUTPUT);
-  pinMode(pirPin1, INPUT);
-  attachInterrupt(digitalPinToInterrupt(pirPin1), pirInterrupt1, CHANGE); // interrupt for PIR sensor 1
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(motionSensorPin, INPUT);
 
-  pinMode(ledPin2, OUTPUT);
-  pinMode(pirPin2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(pirPin2), pirInterrupt2, CHANGE); // interrupt for PIR sensor 2
+  attachInterrupt(digitalPinToInterrupt(motionSensorPin), NewInter, CHANGE);
 
   Serial.begin(9600);
 }
 
-void loop()
-{
-  // The loop is empty for applying interrupts
+void loop() {
+  // The loop is empty to implement interrupt
 }
 
-void pirInterrupt1()
-{
-  if (digitalRead(pirPin1) == HIGH)
-  {
-    digitalWrite(ledPin1, HIGH);
-    Serial.println("PIR Sensor 1 is HIGH - LED 1 is HIGH");
-    delay(1000);
-  }
-  else
-  {
-    digitalWrite(ledPin1, LOW);
-    Serial.println("PIR Sensor 1 is LOW - LED 1 is LOW");
-    delay(1000);
-  }
-}
+void NewInter() {
+  int motionState = digitalRead(motionSensorPin);
+  int temperatureValue = analogRead(temperatureSensorPin);
+  float voltage = (temperatureValue / 1024.0) * 5.0; // It convert analog reading to voltage
+  float temperatureCelsius = (voltage - 0.5) * 100; // It convert voltage to Celsius
 
-void pirInterrupt2()
-{
-  if (digitalRead(pirPin2) == HIGH)
-  {
-    digitalWrite(ledPin2, HIGH);
-    Serial.println("PIR Sensor 2 is HIGH - LED 2 is HIGH");
+  if (motionState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    Serial.print("The LED is on");
+    Serial.print("Temperature: ");
+    Serial.print(temperatureCelsius);
+    Serial.println(" °C");
     delay(1000);
-  }
-  else
-  {
-    digitalWrite(ledPin2, LOW);
-    Serial.println("PIR Sensor 2 is LOW - LED 2 is LOW");
+  } else {
+    digitalWrite(ledPin, LOW);
+    Serial.println("The LED is off");
     delay(1000);
   }
 }
