e42cf6c889639e5aad55e0d47c1c4d03a16cead0
AkshayKumar20043 <141328267+AkshayKumar20043@users.noreply.github.com>
2024-02-26 14:55:40 +0530

    Update code.cpp

--- Module 1/Task M1_T4D code/code.cpp
+++ Module 1/Task M1_T4D code/code.cpp
@@ -1,81 +1,142 @@
-int ledPin1 = 6; // LED connected to digital pin 6
-int pirPin1 = 3; // PIR sensor 1 connected to digital pin 3
-int ledPin2 = 7; // LED connected to digital pin 7
-int pirPin2 = 2; // PIR sensor 2 connected to digital pin 2
+// Define LED pin numbers
+int led1 = 6;
+int led2 = 7;
+int led3 = 8;
+int led4 = 9;
 
-volatile bool pirState1 = LOW; // PIR sensor 1 state
-volatile bool pirState2 = LOW; // PIR sensor 2 state
+// Define PIR and Soil Moisture sensor pin numbers
+int pir1 = 2; // PIR sensor 1
+int pir2 = 3; // PIR sensor 2
+int soilmoisture3 = 4; // Soil Moisture sensor
 
-int ledPinTimer = 8; // Additional LED is connected to digital pin 8 for timer interrupt
+// Variables to store the state of PIR sensors and Soil Moisture sensor
+int pirState1 = LOW;
+int pirState2 = LOW;
+int soilMoistureState = LOW;
 
-void setup()
-{
-  pinMode(ledPin1, OUTPUT);
-  pinMode(pirPin1, INPUT);
+void setup() {
+  // Set LED pins as OUTPUT
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 
-  pinMode(ledPin2, OUTPUT);
-  pinMode(pirPin2, INPUT);
+  // Set sensor pins as INPUT
+  pinMode(pir1, INPUT); // PIR sensor 1
+  pinMode(pir2, INPUT); // PIR sensor 2
+  pinMode(soilmoisture3, INPUT); // Soil Moisture sensor
 
-  pinMode(ledPinTimer, OUTPUT);
-
-  PCMSK0 |= (1 << PCINT3); // Enable PCINT for digital pin 3 (pirPin1)
-  PCMSK0 |= (1 << PCINT2); // Enable PCINT for digital pin 2 (pirPin2)
-  PCICR |= (1 << PCIE0);    // Enable PCINT0 interrupt
-
-  noInterrupts(); // Disable interrupts during configuration
-  TCCR1A = 0;      // Set entire TCCR1A register to 0
-  TCCR1B = 0;      // Set entire TCCR1B register to 0
-  TCNT1 = 0;       // Initialize counter value to 0
-  OCR1A = 15625;    // Set compare match value (1 second at 16 MHz)
-  TCCR1B |= (1 << WGM12); // Configure timer in CTC mode
-  TCCR1B |= (1 << CS12) | (1 << CS10); // Set prescaler to 1024 and start the timer
-  TIMSK1 |= (1 << OCIE1A); // Enable Timer1 compare match interrupt
-  interrupts(); // Enable global interrupts
-
+  // Start serial communication
   Serial.begin(9600);
+  Serial.println("Setup complete");
+
+  // Attach interrupts for PIR sensors and Soil Moisture sensor
+  attachInterrupt(digitalPinToInterrupt(pir1), motionDetected1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pir2), motionDetected2, CHANGE);
+  pciSetup(soilmoisture3);
+
+  // Start timer with a frequency of 0.5 Hz (blinks every 2 seconds)
+  startTimer(1);
 }
 
-void loop()
-{
-  // The loop is empty for applying interrupts
+// Function to set up Pin Change Interrupt for Soil Moisture sensor
+void pciSetup(byte pin) {
+  // it is used to enable or disable individual pins for Pin Change Interrupts.
+  *digitalPinToPCMSK(pin) |= bit(digitalPinToPCMSKbit(pin)); // enable pin
+  PCIFR |= bit(digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+  PCICR |= bit(digitalPinToPCICRbit(pin)); // enable interrupt for the group
 }
 
-ISR(PCINT0_vect)
-{
-  if (digitalRead(pirPin1) == HIGH)
-  {
-    pirState1 = HIGH;
+// Interrupt Service Routine for Soil Moisture sensor
+ISR(PCINT2_vect) {
+  soilMoistureDetected();
+}
+
+// Function to start a timer with a given frequency
+void startTimer(double timerFrequency) {
+  noInterrupts();
+
+  uint16_t ocrValue = (uint16_t)(F_CPU / 1024.0 / timerFrequency - 1);
+
+  TCCR1A = 0; // It keeps track of how the timer should behave.
+  TCCR1B = 0;
+  TCNT1 = 0; // It is used in initializing the counter value to zero.
+  OCR1A = ocrValue; // used for generating an interrupt or changing the state of an output pin.
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  TIMSK1 |= (1 << OCIE1A); // It is used to enable the interrupt for Output Compare A 
+
+  interrupts();
+}
+
+// Interrupt Service Routine for Timer
+ISR(TIMER1_COMPA_vect) {
+  digitalWrite(led4, digitalRead(led4) ^ 1);
+}
+
+// Interrupt Service Routine for PIR sensor 1
+void motionDetected1() {
+  int val = digitalRead(pir1);
+  if (val == HIGH) {
+    digitalWrite(led1, HIGH);
+    if (pirState1 == LOW) {
+      Serial.println("Motion detected on PIR 1!");
+      Serial.println("LED ON");
+      pirState1 = HIGH;
+    }
+  } else {
+    digitalWrite(led1, LOW);
+    if (pirState1 == HIGH) {
+      Serial.println("Motion ended on PIR 1!");
+      Serial.println("LED OFF");
+      pirState1 = LOW;
+    }
   }
-  else
-  {
-    pirState1 = LOW;
-  }
+}
 
-  if (digitalRead(pirPin2) == HIGH)
-  {
-    pirState2 = HIGH;
-  }
-  else
-  {
-    pirState2 = LOW;
+// Interrupt Service Routine for PIR sensor 2
+void motionDetected2() {
+  int val = digitalRead(pir2);
+  if (val == HIGH) {
+    digitalWrite(led2, HIGH);
+    if (pirState2 == LOW) {
+      Serial.println("Motion detected on PIR 2!");
+      Serial.println("LED ON");
+      pirState2 = HIGH;
+    }
+  } else {
+    digitalWrite(led2, LOW);
+    if (pirState2 == HIGH) {
+      Serial.println("Motion ended on PIR 2!");
+      Serial.println("LED OFF");
+      pirState2 = LOW;
+    }
   }
 }
 
-ISR(TIMER1_COMPA_vect)
-{
-  digitalWrite(ledPinTimer, !digitalRead(ledPinTimer)); // Toggle the state of the LED connected to pin 8
+// Interrupt Service Routine for Soil Moisture sensor
+void soilMoistureDetected() {
+  Serial.println("Soil Moisture interrupt triggered");
+  int val = digitalRead(soilmoisture3);
+  Serial.println("Soil Moisture Value: " + String(val));
+
+  if (val == HIGH) {
+    digitalWrite(led3, HIGH);
+    if (soilMoistureState == LOW) {
+      Serial.println("Soil Moisture detected!");
+      Serial.println("LED ON");
+      soilMoistureState = HIGH;
+    }
+  } else {
+    digitalWrite(led3, LOW);
+    if (soilMoistureState == HIGH) {
+      Serial.println("Soil Moisture ended!");
+      Serial.println("LED OFF");
+      soilMoistureState = LOW;
+    }
+  }
 }
 
-void handlePIRState1()
-{
-  digitalWrite(ledPin1, pirState1);
-  Serial.print("PIR Sensor 1 is ");
-  Serial.println(pirState1 ? "HIGH" : "LOW");
-}
-
-void handlePIRState2()
-{
-  digitalWrite(ledPin2, pirState2);
-  Serial.print("PIR Sensor 2 is ");
-  Serial.println(pirState2 ? "HIGH" : "LOW");
+void loop() {
+  delay(1000); // Add any delay or other code here if needed
 }
